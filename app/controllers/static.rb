63124de2bdb9f0a5711222b1b43e8dc2eec7fb7a
Khairul <karipish@gmail.com>
2016-04-19 19:25:04 +0800

    First commit

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -3,14 +3,14 @@ require 'byebug'
 get '/' do
 	puts "[LOG] Getting /"
 	puts "[LOG] Params #{params.inspect}"
-  erb :"static/index"
+  erb :"layouts/application"
 end
 
 post '/urls' do
 	@url = Url.create(long_url: params[:long_url])
 	@url.shorten
 	@url.save
-	erb :"static/index"
+	erb :"layouts/application"
 end
 
 get '/:short_url' do
@@ -23,3 +23,9 @@ get '/:short_url' do
 	redirect to('http://' + url.long_url)
 end
 
+
+
+# <!-- 	<% unless @url.nil? %> 
+# 	<p> You have inluded <%= @url.long_url %> into the database. </p>
+# 	<p> Your short URL:<a href="<%= @url.short_url %>"><%=@url.short_url %></a></p>
+# 	<% end %> -->
